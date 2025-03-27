a616436641deb5b1913a1aa0f098b77530d18495
j0hanz <golfarenlj@gmail.com>
2025-03-27 14:14:03 +0100

    Add templates configuration to settings

--- api/settings.py
+++ api/settings.py
@@ -79,6 +79,23 @@ ROOT_URLCONF = 'api.urls'
 
 WSGI_APPLICATION = 'api.wsgi.application'
 
+# Templates configuration
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Database configuration
 if 'DEV' in os.environ:
     DATABASES = {
