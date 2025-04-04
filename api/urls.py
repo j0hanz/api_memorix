ab3d7c364c186930c75a7de605d949d4e5516fc9
j0hanz <golfarenlj@gmail.com>
2025-04-04 09:05:29 +0200

    Add JWT token obtain and refresh endpoints to API URLs

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,10 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import RootRoute
 
@@ -9,4 +13,10 @@ urlpatterns = [
     path('', RootRoute.as_view()),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
+    path(
+        'api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'
+    ),
+    path(
+        'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'
+    ),
 ]
