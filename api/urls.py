486f7bcf5dfdd0609e6e009a9b3f8ffab570707b
j0hanz <golfarenlj@gmail.com>
2025-04-05 00:08:55 +0200

    Refactor URL patterns to streamline authentication routes and improve organization

--- api/urls.py
+++ api/urls.py
@@ -2,21 +2,17 @@
 
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 from .views import RootRoute
 
 urlpatterns = [
     path('', RootRoute.as_view()),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/',
+        include('dj_rest_auth.registration.urls'),
+    ),
     path('api/', include('users.urls')),
-    path(
-        'api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'
-    ),
-    path(
-        'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'
-    ),
 ]
