8e03796f229d27d1477eb3a7b93979fb825582e1
j0hanz <golfarenlj@gmail.com>
2025-04-05 23:03:12 +0200

    Remove redundant error handling in RootRoute view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import permissions
-from rest_framework.exceptions import APIException
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,8 +10,4 @@ class RootRoute(APIView):
     permission_classes = [permissions.AllowAny]
 
     def get(self, request: Request) -> Response:
-        try:
-            return Response({'message': 'Memorix API is running!'})
-        except Exception as e:
-            msg = f'An error occurred: {str(e)}'
-            raise APIException(msg)
+        return Response({'message': 'Memorix API is running!'})
