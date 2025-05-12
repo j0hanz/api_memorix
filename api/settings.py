846243f568d2a0c19d5ee0181b9f28f5645f504f
j0hanz <golfarenlj@gmail.com>
2025-05-12 10:49:51 +0200

    Remove default pagination settings from REST framework configuration

--- api/settings.py
+++ api/settings.py
@@ -138,8 +138,6 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.MultiPartParser',
     ],
     'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
 }
 
 if DEBUG:
