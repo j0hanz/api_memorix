67545856f46485a3edb6f3191dc5559dff24a8b2
j0hanz <golfarenlj@gmail.com>
2025-05-12 11:08:47 +0200

    Add pagination settings to REST framework configuration

--- api/settings.py
+++ api/settings.py
@@ -138,6 +138,8 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.MultiPartParser',
     ],
     'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 if DEBUG:
